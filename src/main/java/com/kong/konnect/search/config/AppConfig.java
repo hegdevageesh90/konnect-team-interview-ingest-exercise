22e5891144e346827f8ea95a71d7d39850ac845f
hegdevageesh <hegdevageesh@MacBooks-MacBook-Air.local>
2024-10-02 23:15:40 +0530

    remove unnecessary code

--- src/main/java/com/kong/konnect/search/config/AppConfig.java
+++ src/main/java/com/kong/konnect/search/config/AppConfig.java
@@ -4,10 +4,17 @@ import com.kong.konnect.search.config.properties.KafkaProperties;
 import com.kong.konnect.search.config.properties.OpenSearchProperties;
 import org.apache.http.HttpHost;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
 
 /**
  * Generates necessary beans.
@@ -16,20 +23,10 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class AppConfig {
-  private final KafkaProperties kafkaProperties;
   private final OpenSearchProperties openSearchProperties;
 
-  public AppConfig(KafkaProperties kafkaProperties, OpenSearchProperties openSearchProperties) {
-    this.kafkaProperties = kafkaProperties;
+  public AppConfig(OpenSearchProperties openSearchProperties) {
     this.openSearchProperties = openSearchProperties;
-  }
-
-  @Bean
-  public NewTopic konnectTopic() {
-    return new NewTopic(
-        kafkaProperties.getTopicName(),
-        kafkaProperties.getPartitions(),
-        kafkaProperties.getReplicationFactor());
   }
 
   @Bean
@@ -40,5 +37,19 @@ public class AppConfig {
                 openSearchProperties.getHost(),
                 openSearchProperties.getPort(),
                 openSearchProperties.getScheme())));
+  }
+
+  @Bean
+  public ProducerFactory<String, String> producerFactory() {
+    var configProps = new HashMap<String, Object>();
+    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    return new DefaultKafkaProducerFactory<>(configProps);
+  }
+
+  @Bean
+  public KafkaTemplate<String, String> kafkaTemplate() {
+    return new KafkaTemplate<>(producerFactory());
   }
 }
